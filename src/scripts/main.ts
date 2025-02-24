7560b52aa4f12bf5936f4db699b12af12545fcf8
ZeetPatel2712 <zeetpatel2712@gmail.com>
2025-02-24 23:21:44 +0530

    completed design part

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,25 @@
 import '../styles/globals.css';
+import 'iconify-icon';
+
+const todoForm = document.querySelector('form');
+const todoInput = document.querySelector<HTMLInputElement>('#todo-input');
+const addTodoBtn = document.querySelector('#add-todo');
+const todoList = document.querySelector('#todo-list');
+
+let allTodos: string[] = [];
+
+todoForm?.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTodo();
+});
+
+const addTodo = () => {
+    const todoText = todoInput?.value.trim();
+    if (todoText && todoText.length > 0) {
+        allTodos.push(todoText);
+        console.log(allTodos);
+        if (todoInput) {
+            todoInput.value = '';
+        }
+    }
+};
