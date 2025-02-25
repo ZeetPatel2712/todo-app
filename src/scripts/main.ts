9ae4f30c13b9b9b4c0cee2fb0c4b9b7e46bdae58
ZeetPatel2712 <zeetpatel2712@gmail.com>
2025-02-25 21:41:54 +0530

    make todo app working

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -10,16 +10,38 @@ let allTodos: string[] = [];
 
 todoForm?.addEventListener('submit', (e) => {
     e.preventDefault();
-    addTodo();
-});
-
-const addTodo = () => {
     const todoText = todoInput?.value.trim();
     if (todoText && todoText.length > 0) {
         allTodos.push(todoText);
-        console.log(allTodos);
+        updateTodoList();
         if (todoInput) {
             todoInput.value = '';
         }
     }
+});
+
+const updateTodoList = () => {
+    if (todoList) {
+        todoList.innerHTML = '';
+        allTodos.forEach((todo, todoIndex) => {
+            const todoItem = createTodoItem(todo, todoIndex);
+            todoList.appendChild(todoItem);
+        });
+        console.log(allTodos);
+    }
+};
+
+const createTodoItem = (todo: string, index?: number) => {
+    const todoLI = document.createElement('li');
+    todoLI.className = 'flex items-center gap-3 mb-3';
+    todoLI.innerHTML = `<input type="checkbox" id="todo-${index}" class="peer
+                    accent-indigo-500 cursor-pointer hover:accent-indigo-400
+                    h-4 w-4" />
+                    <label for="todo-${index}" class="peer-checked:line-through cursor-pointer">${todo}</label>
+                    <button id="delete-btn" class="ms-auto leading-0
+                    cursor-pointer hover:text-red-500 duration-300">
+                        <iconify-icon icon="material-symbols:delete-rounded" width="22" height="22"></iconify-icon>
+                    </button>`;
+
+    return todoLI;
 };
